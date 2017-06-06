8559199da12269a770248fcf79336884af652c9a
Mario  Andrade <mario@businessconfig.com>
2017-06-06 18:25:21 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+    return gulp.src('app/assets/_scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/assets/css/'));
+});
+
+gulp.task('default', ['sass'], function() {
+    gulp.watch('app/assets/_scss/*.scss', ['sass']);
+});
