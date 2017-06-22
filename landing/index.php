612794d2ce22eb48d63da0d859e857002ede3353
renshipt <laurentino@renshi.pt>
2017-06-22 16:44:11 +0100

    Landing Page
    Comming Soon Homepage

---
+++ landing/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Front Files - Comming Soon</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/full.css?<?php echo date('Ymdhis'); ?>" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body class="full">
+    
+    <header>
+        <div class="logo"><img src="img/logo.png" /></div>
+    </header>
+ 
+    <footer class="footer">
+      <div class="widgets">
+          <div class="col-sm-4 col-xs-12 widget first">
+              <div class="widget-content">
+                  The Global Platform for Independent Journalism
+              </div>
+          </div>
+          <div class="col-sm-5 col-xs-12 widget second">
+              <div class="widget-content">
+                  <br/>Great news coming soon
+              </div>
+          </div>
+          <div class="col-sm-3 col-xs-12 widget third">
+              <div class="widget-content">
+                  <br/><a href="mailto:info@frontfiles.com">info@frontfiles.com</a>
+              </div>
+              </div>
+      </div>
+    </footer>
+    
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
