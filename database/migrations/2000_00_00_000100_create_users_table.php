b230b04e66498bd941454f46dcae203eba8bc597
rui <ruihortafigueiredo@gmail.com>
2017-07-13 11:34:08 +0100

    fixed migration issues

--- database/migrations/2000_00_00_000100_create_users_table.php
+++ database/migrations/2000_00_00_000100_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table){
             $table->bigIncrements('id');
-            $table->unsignedSmallInteger('role_id');
+            $table->unsignedSmallInteger('role_id')->default(1);
 
             $table->string('name');
             $table->string('email')->unique();
