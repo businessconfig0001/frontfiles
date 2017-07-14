fd301e005413ef1eb59d18facd339608a16f0043
andrefcbotelho <andre.vmz@hotmail.com>
2017-07-14 19:04:43 +0100

    :wrench: mailchimp newsletter

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -23,8 +23,26 @@
 	                	</div>
                 		<div class="col-sm-6 col-xs-12"></div>
                 		<div class="col-sm-8 col-xs-12 mailing">
-                			<input type="text" placeholder="E-mail">
-                			<input type="submit" class="bg-blue" value="SEND">
+
+							<!-- Begin MailChimp Signup Form -->
+							<div id="mc_embed_signup">
+								<form action="//mdklive.us11.list-manage.com/subscribe/post?u=dfeda9ebea80059ddcd76838f&amp;id=2deb7e2552" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+									<div id="mc_embed_signup_scroll">
+
+										<div class="mc-field-group">
+											<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="E-mail">
+										</div>
+										<div id="mce-responses" class="clear">
+											<div class="response" id="mce-error-response" style="display:none"></div>
+											<div class="response" id="mce-success-response" style="display:none"></div>
+										</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+										<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_dfeda9ebea80059ddcd76838f_2deb7e2552" tabindex="-1" value=""></div>
+										<div class="clear"><input type="submit" value="SEND" name="subscribe" id="mc-embedded-subscribe" class="bg-blue"></div>
+									</div>
+								</form>
+							</div>
+							<!--End mc_embed_signup-->
+
                 		</div>
                 		<div class="col-sm-4 col-xs-12"></div> 
                 	</div>
