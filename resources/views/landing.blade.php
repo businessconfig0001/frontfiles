ce29d84de87cc32d2435e059c4b28eb537b6e799
Milan De Buck <milan.de.buck@telenet.be>
2017-07-14 20:19:14 +0200

    newsletter + MENU

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -30,14 +30,14 @@
 									<div id="mc_embed_signup_scroll">
 
 										<div class="mc-field-group">
-											<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="E-mail">
+											<input type="email" value="" name="EMAIL" class="required email form-control newsletter" id="mce-EMAIL" placeholder="E-mail">
 										</div>
 										<div id="mce-responses" class="clear">
 											<div class="response" id="mce-error-response" style="display:none"></div>
 											<div class="response" id="mce-success-response" style="display:none"></div>
 										</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 										<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_dfeda9ebea80059ddcd76838f_2deb7e2552" tabindex="-1" value=""></div>
-										<div class="clear"><input type="submit" value="SEND" name="subscribe" id="mc-embedded-subscribe" class="bg-blue"></div>
+										<div class="clear"><input type="submit" value="SEND" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary newsletter"></div>
 									</div>
 								</form>
 							</div>
