afffede9088bdc9f08df2a22f0b8149f23cde74b
Milan De Buck <milan.de.buck@telenet.be>
2017-07-24 12:07:36 +0200

    profile page designn

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -2,31 +2,37 @@
 
 @section('content')
 
-    <div class="container">
+    <div class="container profile">
 
-        <div class="row">
-            <h1>{{ $user->name }}</h1>
+        <div class="row info">
+            <h1 class="title">{{ $user->name }}</h1>
         </div>
 
-        <div class="row">
-            @if($user->google_clientId && $user->google_clientSecret && $user->google_refreshToken && $user->google_folderId)
-                <p>Google Drive configured!</p>
-            @else
-                <p>Google Drive not configured!<br>
-                    Please configure it so that you can save files in your Google Drive.
-                </p>
-            @endif
-            @if($user->dropbox_token && $user->dropbox_app_name)
-                <p>Dropbox configured!</p>
-            @else
-                <p>Dropbox not configured!<br>
-                    Please configure it so that you can save files in your Dropbox.
-                </p>
-            @endif
-        </div>
-
-        <div class="row">
-                <a href="{{ route('profile.edit') }}" class="btn btn-primary">Edit</a>
+        <div class="row storage">
+            <ul>
+                <li class="box">
+                    <img  class="logo" src="/images/googledrive.png" alt="">
+                    <h3 class="title">Google Drive</h3>
+                    @if($user->google_clientId && $user->google_clientSecret && $user->google_refreshToken && $user->google_folderId)
+                        <p>Google Drive configured!</p>
+                    @else
+                        <p>Google Drive not configured!</p>
+                        <a href="{{ route('profile.edit') }}" class="btn btn-primary">Configure Google Drive</a>
+                    @endif
+                </li>
+                <li class="box">
+                    <img  class="logo" src="/images/dropbox_glyph_blue.svg" alt="">
+                    <h3 class="title">Dropbox</h3>
+                    @if($user->dropbox_token && $user->dropbox_app_name)
+                        <p>Dropbox configured!</p>
+                    @else
+                        <p>Dropbox not configured!</p>
+                        <a href="{{ route('profile.edit') }}" class="btn btn-primary">Configure Dropbox</a>
+                    @endif
+                </li>
+            </ul>
+            
+            
         </div>
 
     </div>
