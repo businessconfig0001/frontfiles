3e3bd21981b9fdfa466df93307d9a13f1c355bd2
André Botelho <andre.vmz@hotmail.com>
2017-08-18 03:54:49 +0100

    :wrench: fetching video from dropbox

--- app/Jobs/FileTypes/Video.php
+++ app/Jobs/FileTypes/Video.php
@@ -4,6 +4,9 @@ namespace FrontFiles\Jobs\FileTypes;
 
 use FrontFiles\File;
 use FrontFiles\Jobs\Interfaces\FileProcessInterface;
+use FrontFiles\Utility\DriversHelper;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+
 
 class Video implements FileProcessInterface
 {
@@ -11,10 +14,17 @@ class Video implements FileProcessInterface
      * Method to process the file.
      *
      * @param File $file
+     * @throws FileNotFoundException
      */
     public function process(File $file)
     {
         //fetch from dropbox
+        $filesystem = DriversHelper::userDropbox($file->owner->dropbox_token);
+
+        if(!$filesystem->has($file->name))
+            throw new FileNotFoundException('We couln\'t find this file!');
+
+        $video = $filesystem->read($file->name);
 
         //add watermark and encoding
 
