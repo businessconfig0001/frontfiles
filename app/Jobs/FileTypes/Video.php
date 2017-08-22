2c5f36216f5a40d54413fb41dbd06bce4cc61521
André Botelho <andre.vmz@hotmail.com>
2017-08-22 04:27:18 +0100

    :wrench: encoding and weatermark

--- app/Jobs/FileTypes/Video.php
+++ app/Jobs/FileTypes/Video.php
@@ -6,7 +6,10 @@ use FrontFiles\File;
 use FrontFiles\Jobs\Interfaces\FileProcessInterface;
 use FrontFiles\Utility\DriversHelper;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
-
+use Pbmedia\LaravelFFMpeg\FFMpeg;
+use MicrosoftAzure\Storage\Common\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\{ CreateContainerOptions, PublicAccessType };
+use WindowsAzure\Common\ServicesBuilder;
 
 class Video implements FileProcessInterface
 {
@@ -24,19 +27,53 @@ class Video implements FileProcessInterface
         if(!$filesystem->has($file->name))
             throw new FileNotFoundException('We couln\'t find this file!');
 
-        $video = $filesystem->read($file->name);
+        $container = 'user-id-' . auth()->user()->id;
+        $config = config('filesystems.default');
+
+        if($config === 'azure')
+            $this->createContainerIfNeeded($container);
+
+        $new_name = 'processed_' . $file->name;
 
         //add encoding
-
-
         //add watermark
-
-        //save to blob storage
+        FFMpeg::fromFilesystem($filesystem)
+            ->open($file->name)
+            ->addFilter(['-i', asset('images/logo2x.png'),'-filter_complex','overlay=10:10'])
+            ->addFilter(['-strict', 1])
+            ->export()
+            ->toDisk(config('filesystems.default'))
+            ->inFormat(new \FFMpeg\Format\Video\X264('libmp3lame', 'libx264'))
+            ->save($new_name);
 
         //save blob storage url in "azure_url" field, for previews
-
         //change value "processed" to true
+        $file->update([
+            'azure_url' => config('filesystems.disks.' . $config . '.url') . $new_name,
+            'processed' => true, //TODO AQUIII
+        ]);
 
         //warn user that video has been processed?
+    }
+
+    /*
+     * Checks if the current user container exists in the azure blob storage
+     * If it does not exist, he creates it.
+     *
+     * @param string $container
+     */
+    protected function createContainerIfNeeded(string $container)
+    {
+        //BLOBS_ONLY means that its public
+        $containerOptions = new CreateContainerOptions();
+        $containerOptions->setPublicAccess(PublicAccessType::BLOBS_ONLY);
+
+        try{
+            ServicesBuilder::getInstance()
+                ->createBlobService(config('filesystems.disks.azure.endpoint'))
+                ->createContainer($container, $containerOptions);
+        }catch(ServiceException $e){
+            //If there's an exception, it means that the container (folder) already exists
+        }
     }
 }
