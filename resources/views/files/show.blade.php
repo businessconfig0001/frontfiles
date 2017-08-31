4f48e98397857f89b9890f89c8382756ee8b6b7f
Milan De Buck <milan.de.buck@telenet.be>
2017-08-31 12:14:38 +0200

    css changes

--- resources/views/files/show.blade.php
+++ resources/views/files/show.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <div class="show-container">
-        <file-detail :file="{{$file}}"></file-detail>
+    <div class="container">
+        <div class="show-container">
+            <file-detail :file="{{$file}}"></file-detail>
+        </div> 
     </div>
-
 @endsection
