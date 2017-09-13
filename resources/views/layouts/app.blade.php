c2087c1a4d4f59f2784be9ae3d8afeebc1c1db2e
andrefcbotelho <andre.vmz@hotmail.com>
2017-09-13 18:27:18 +0100

    :bug: favicon fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{ url('/css/app.css') }}">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
         <script>
             window.csrfToken = "{{ csrf_token() }}";
         </script>
