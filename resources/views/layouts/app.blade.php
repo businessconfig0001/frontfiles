87281b570cfe65bd84a86034ad2151ce1862103a
Milan De Buck <milan.de.buck@telenet.be>
2017-09-13 17:20:47 +0200

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{ url('/css/app.css') }}">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="icon" href="favicon.ico" type="image/x-icon"/>
         <script>
             window.csrfToken = "{{ csrf_token() }}";
         </script>
