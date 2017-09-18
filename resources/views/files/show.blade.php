533cdfa42413bea0b3d7dcc1a95b3ea8ebf0a757
Milan De Buck <milan.de.buck@telenet.be>
2017-09-18 12:28:50 +0200

    fix

--- resources/views/files/show.blade.php
+++ resources/views/files/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 	@if (Auth::guest())
-		<file-detail :fileprop="{{$file}}" :author="{{$file->owner->fullNameAndLocation()}}"></file-detail>
+		<file-detail :fileprop="{{$file}}" :author="'{{$file->owner->fullNameAndLocation()}}'"></file-detail>
 	@else
 		<file-detail :fileprop="{{$file}}" :user="{{ Auth::user() }}" :author="'{{$file->owner->fullNameAndLocation()}}'"></file-detail>
 	@endif
