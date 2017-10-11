858f38e638436512ebc49a21170a5deb68661f73
Milan De Buck <milan.de.buck@telenet.be>
2017-10-11 19:47:31 +0200

    fixed the http map error

--- resources/assets/js/components/maps/map-component.vue
+++ resources/assets/js/components/maps/map-component.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-const d3 = require('d3')
+import { map } from './../../services/mapService'
 export default {
 
 	name: 'map-component',
@@ -22,31 +22,7 @@ export default {
 		}
 	},
 	mounted(){
-		this.svg=d3.select(this.$refs.map)
-		let width=parseInt(this.svg.style("width"))
-		let height=parseInt(this.svg.style("height"))
-		console.log(this.svg,width,height)
-		let projection=d3.geoMercator()
-	      //.scale(width / 2 / Math.PI)
-	      .translate([width / 2 - 30, height/2  + 210])
-		let path=d3.geoPath().projection(projection)
-
-		let url = "http://enjalot.github.io/wwsd/data/world/world-110m.geojson"
-	    d3.json(url, (err, geojson) => {
-	      this.svg.append("path")
-	      	.attr("d", path(geojson))
-	      	.attr("fill","blue")
-	      this.svg.selectAll(".mark")
-		    .data(this.data)
-		    .enter()
-		    .append("image")
-		    .attr('class','mark')
-		    .attr('width', 20)
-		    .attr('height', 20)
-		    .attr("xlink:href",'https://cdn3.iconfinder.com/data/icons/softwaredemo/PNG/24x24/DrawingPin1_Blue.png')
-		    .attr("transform", d => "translate(" + projection([d.longitude,d.latitude]) + ")")
-	    })
-
+		map(this.$refs.map,this.data)
 	}
 };
 </script>
---
+++ resources/assets/js/services/mapService.js
@@ -0,0 +1,80 @@
+const url="./world.geojson"
+const d3 = require('d3')
+let projection
+
+function drawMap(el){
+	return new  Promise((resolve) => {
+		let width=parseInt(el.style("width"))
+		let height=parseInt(el.style("height"))
+		projection=d3.geoMercator()
+	      //.scale(width / 2 / Math.PI)
+	      .translate([width / 2 - 30, height/2  + 210])
+		let path=d3.geoPath().projection(projection)
+
+	    d3.json(url, (err, geojson) => {
+	      el.append("path")
+	      	.attr("d", path(geojson))
+	      	.attr("fill","blue")
+	      	resolve()
+	     })
+	})
+	
+}
+
+function createMarkers(data){
+	
+	let coors=data.map(d => {
+		return {
+			lat:d.latitude,
+			lng:d.longitude
+		}
+	})
+	console.log(coors)
+	let markers =[]
+	for(let i = 0; i < coors.length ; i++){
+		let c=coors[i]
+		let contains=false;
+		for(let j = 0; j < markers.length ; j++){
+			let m=markers[j]
+			console.log(m.lat,c.lat,m.lng,c.lng)
+			if(m.lat === c.lat && m.lng === c.lng){
+				contains=true
+				m.count++;	
+			} 
+		}
+		if(!contains)markers.push({
+			lat:c.lat,
+			lng:c.lng,
+			count:1
+		})
+	}
+	return markers
+
+}
+function drawMarkers(markers,el){
+	el.selectAll(".mark")
+		    .data(markers)
+		    .enter()
+		    .append("circle")
+		    .attr('fill','black')
+		    .attr('r', 10)
+		    .attr("transform", d => "translate(" + projection([d.lng,d.lat]) + ")")
+		    .append('text')		    
+		    .attr("text-anchor", "middle") 
+		    .style('fill','white')
+		    .style('font-size','8px')
+		    .text(d => d.count)
+
+}
+
+function map(el,data){
+	el=d3.select(el)
+	drawMap(el).then(() => {
+		drawMarkers(createMarkers(data),el)	
+	})
+	
+}
+
+export {
+	map
+}
