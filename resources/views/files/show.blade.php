5aa7990eb7dd3828b487b35545bfb977e9879954
andrefcbotelho <andre.vmz@hotmail.com>
2017-10-31 15:27:59 +0000

    :wrench: og tags fix

--- resources/views/files/show.blade.php
+++ resources/views/files/show.blade.php
@@ -2,11 +2,15 @@
 
 @section('fb-data')
 <!-- Facebook Open Graph -->
-		<meta property="og:title" 		content="FrontFiles">
-		<meta property="og:description" content="{{ $file->title }}">
-		<meta property="og:url" 		content="{{ url()->current() }}">
-        <meta property="og:type" 		content="article">
-        <meta property="og:image" 		content="{{ asset('images/ff_generic_fb_logo.png') }}">
+		<meta property="og:site_name" 			content="FrontFiles">
+		<meta property="og:title" 				content="{{ $file->title }}">
+		<meta property="og:description" 		content="{{ $file->description }}">
+		<meta property="og:url" 				content="{{ url()->current() }}">
+        <meta property="og:type" 				content="article">
+        <meta property="og:image" 				content="{{ asset('images/ff_generic_fb_logo.png') }}">
+		<meta property="og:locale" 				content="en_GB">
+		<meta property="og:locale:alternate" 	content="pt_PT">
+		<meta property="og:locale:alternate" 	content="pt_BR">
 @endsection
 
 @section('content')
